02046a0237871c8f9787b8b69a25436e7ab3469e
JDJG <167665838+JDJG3493@users.noreply.github.com>
2024-04-21 01:37:28 -0400

    Update folders.bat

--- folders.bat
+++ folders.bat
@@ -1,2 +1,3 @@
-python %UserProfile%\open_folder.py
+python %UserProfile%\open_folders.py
+echo put it in any folder you want.
 echo you will want to put this into shell:startup to work and change this message if you want and change the directory
